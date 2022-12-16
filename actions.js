3f9f0226b7b9ab2b7f50a5c62282e5d256a498f2
Jett <jettc505@gmail.com>
2022-12-16 10:13:09 -0700

    working on the meny

--- actions.js
+++ actions.js
@@ -1,43 +1,62 @@
+const mysql = require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "company_db"
+    }
+);
+// db.query("INSERT INTO employees (id) VALUES(6)",(err,result) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(result)
+// });
+
 // View functions
 
-function viewDeparts () {
+function viewDeparts(departsName) {
     // Displays the departments to the user
+    const query = db.query("SELECT * FROM departments")
 }
 
-function viewRoles () {
+function viewRoles() {
     // Displays the Roles to the user
 }
 
-function viewEmploys () {
+function viewEmployees() {
     // Displays the Employees to the user
 }
 
 // Add Functions
 
-function addDepart () {
+function addDepart() {
     // Allows the user to add a department
 }
 
-function addRole () {
+function addRole() {
     // Allows the user to add a role
 }
 
-function addEmploy () {
+function addEmploy() {
     // Allows the user to add an Employee
 }
 
 // Update function
 
-function updateEmpRol () {
+function updateEmpRol() {
     //  Allows the user to update an employees role
 }
 
 module.exports = {
     viewDeparts,
     viewRoles,
-    viewEmploys,
-    addDepart,  
+    viewEmploys: viewEmployees,
+    addDepart,
     addRole,
     addEmploy,
-    updateEmpRol 
-}
+    updateEmpRol
+};
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const inquirer = require("inquirer");
+const actions = require("./actions")
+
+const choice1 = "View all Departments"
+const choice2 = "View all Roles" 
+const choice3 = "View all Employees"
+const choice4 = "Add a Department"
+const choice5 = "Add a Role"
+const choice6 = "Add an Employee"
+const choice7 = "Update an Employee Role"
 
 const choices = [
     {
@@ -6,38 +15,45 @@ const choices = [
         name: "choices",
         message: "Select The Option You Wish To Use.",
         choices: [
-            "View all Departments",
-            "View all Roles",
-            "View all Employees",
-            "Add a Department",
-            "Add a Role",
-            "Add an Employee",
-            "Update an Employee Role"
+            choice1,
+            choice2,
+            choice3,
+            choice4,
+            choice5,
+            choice6,
+            choice7
         ]
     }
 ];
 
-async function menu_test(a,b) {
+async function menu_test() {
     // Displays the menu to the string
 
     await inquirer
         .prompt(choices)
         .then((data) => {
-
+            console.log(data)
+            if (choice1 === data) {
+                console.log("Yes")
+                actions.viewDeparts(data)
+            } else {
+                console.log("Nop")
+            }
+            return data
         }); 
 }
 
-function menu() {
-    // Displays the menu to the string
-    inquirer
-        .prompt(choices)
-        .then((data) => {
-            console.log(data);
-        });
-}
+// function menu() {
+//     // Displays the menu to the string
+//     inquirer
+//         .prompt(choices)
+//         .then((data) => {
+//             console.log(data);
+//         });
+// }
 
-const menu_test2 = inquirer.prompt(choices).then(async (data) => { 
-    await console.log(data)
-})
+// const menu_test2 = inquirer.prompt(choices).then(async (data) => { 
+//     await console.log(data)
+// })
 
-module.exports = menu_test2
+menu_test()
